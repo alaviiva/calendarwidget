0d846571a233bb7b56133065f593659cf6a568e2
Sampo Vartiala <sampo.vartiala@gmail.com>
2017-02-12 19:36:46 +0200

    Initial commit, shows a widget

---
+++ app/src/main/java/alaviiva/calendarwidget/CalendarWidgetProvider.java
@@ -0,0 +1,14 @@
+package alaviiva.calendarwidget;
+
+import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+
+
+public class CalendarWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        int a = 0;
+    }
+
+}
